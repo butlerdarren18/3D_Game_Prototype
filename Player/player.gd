b0f50eadf1298eaf47d50b41b486bda818de1428
Darren Butler <butlerdarren18@gmail.com>
2025-12-10 18:44:10 -0800

    Animations working and basic movement working.

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,11 @@
 extends CharacterBody3D
-
-@export var animation_player : PlayerAnimationPlayer
+@onready var movement_keys : Dictionary = preload("res://Resources/DictionaryResource/movement_key_collection.tres").collection
 @export var is_test_mode : bool = true
 
 var is_sprinting = false
 
 var speed = 5.0
 const JUMP_VELOCITY = 4.5
-
-
-signal movement_state_changed(state_name : String)
 
 func _ready(): 
 	if not is_test_mode: $TestCam.queue_free()
@@ -24,9 +20,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("movement_key_left", "movement_key_right", "movement_key_up", "movement_key_down")
+	var input_dir := Input.get_vector(movement_keys["left"], movement_keys["right"], movement_keys["up"], movement_keys["down"])
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
--- Player/player_animations.gd
+++ Player/player_animations.gd
@@ -1,30 +1,48 @@
 extends AnimationPlayer
-
+class_name PlayerAnimationPlayer
+@export var movement_keys : DictionaryResource 
 ## Changes animations for player. 
 ## Works using parent "movement_state_changed" signal. Parent signal should pass a string. [br]
 ## if no string is passed the engine might give an error. 
-class_name PlayerAnimationPlayer
 
 ## Obvious usage. [br] If not setting check the switch_animations method is being called and has not been altered.
-@export var idle_speed_scale : float = 0.8 
-## Obvious usage. [br] If not setting check the switch_animations method is being called and has not been altered.
-@export var run_speed_scale : float = 1
-## Obvious usage. [br] If not setting check the switch_animations method is being called and has not been altered.
-@export var walk_speed_scale : float = 1
-
+@export var speed_scales : Dictionary = {
+	"walk" : 1,
+	"idle" : 0.8,
+	"run" : 1
+}
 
 ## This was originally set up with the FOSS godot plush character. 
 ##Please feel free to change it in the future when you have your own model. [br][br]
 ## Current keys: "walk", "run", "idle" [br]
 ## Current subkeys: "speed_scale", "name"* [br][br]
 ## Note: "name" should be the name of the animation in the player, otherwise it won't work
-var animations_dictionary : Dictionary[String, Dictionary] = {
-	"walk": {"speed_scale" : walk_speed_scale,
-			 "name"		   : "walk"},
-	"run":  {"speed_scale" : run_speed_scale,
-			  "name"	   : "run"},
-	"idle": {"speed_scale" : idle_speed_scale,
-			 "name"		   : "idle"}
-}
 
-func switch_animations(animation_name : String): pass
+
+
+var animation : String = "idle"
+var backwards 
+func _physics_process(delta: float) -> void:
+	var is_movement_key_pressed : bool = false
+	var inputs = movement_keys.collection
+	
+	# So we know whether to play_in_reverse of not
+	backwards = Input.is_action_pressed("movement_key_down")
+	
+	# Call walk and run if keys are pressed 
+	for key in inputs: 
+		if Input.is_action_pressed(inputs[key]): 
+			is_movement_key_pressed = true 
+			if Input.is_action_pressed("sprint_key"): run()
+			else: walk()
+	if is_movement_key_pressed == false: idle()
+
+func set_animation(animation_name:String):
+	if current_animation != animation_name: 
+		speed_scale = speed_scales[animation_name]
+		if backwards: play_backwards(animation_name)
+		else: play(animation_name)
+
+func walk(): set_animation("walk")
+func run(): set_animation("run")
+func idle(): set_animation("idle")
